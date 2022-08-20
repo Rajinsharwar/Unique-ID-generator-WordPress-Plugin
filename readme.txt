e1f97cf028d8d535fae732308a1235ab21767e57
Rajin Sharwar <rajinsharwar@gmail.com>
2022-08-20 14:29:03 +0600

    Adding the plugin files

---
+++ readme.txt
@@ -0,0 +1,18 @@
+Unique Customer ID Generator & Displayer
+
+== Description ==
+This plugin generates an unique customert ID for every registered user, and displays them while they are logged in. You can choose to display the ID any where and everywhere suing either a simple shortcode, or a simple function.
+
+
+
+** How to use it on posts or pages?
+
+Use the shortcode `[customer-id]`
+
+
+** How can I use it in my theme?
+
+You can call the function get_current_usser_id()
+To display it, you can paste the below snippet:
+<?php print get_current_user_id(); ?>
+
---
+++ uniqueidgenerator.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+* Plugin Name:       Unique Customer ID Generator & Displayer
+* Plugin URI:        https://ravlet.com
+* Description:       Dsiplay Unique Custpomer IDs for every user. Use it to display anywhere using a simple shortcode/function. refer to the readme.txt attached for more information on how to use the plugin.
+* Version:           1.0.0
+* Author:            rajin Sharwar
+* Author URI:        https://linkedin.com/in/rajinsharwar
+*/
+
+
+function customer_id() {
+	if ( is_user_logged_in() ) {
+		return get_current_user_id();
+	}
+	else {}
+}
+add_shortcode( 'customer-id', 'customer_id' );
