76282287377ef5e438e58ac87508985b797eb5dc
Rajin Sharwar <rajinsharwar@gmail.com>
2023-01-05 16:03:19 +0600

    Update uniqueidgenerator.php

--- uniqueidgenerator.php
+++ uniqueidgenerator.php
@@ -2,10 +2,10 @@
 
 /*
 * Plugin Name:       Unique Customer ID Generator & Displayer
-* Plugin URI:        https://ravlet.com
+* Plugin URI:        https://example.com
 * Description:       Dsiplay Unique Custpomer IDs for every user. Use it to display anywhere using a simple shortcode/function. refer to the readme.txt attached for more information on how to use the plugin.
 * Version:           1.0.0
-* Author:            rajin Sharwar
+* Author:            Rajin Sharwar
 * Author URI:        https://linkedin.com/in/rajinsharwar
 */
 
